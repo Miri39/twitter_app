e83910d9e304f3f69def7eaf5a06148dc07a7efe
Miri <mirialin.39@gmail.com>
2022-04-01 16:41:28 +0300

    primul meu commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::Base
+    def helloblabla
+        render html: 'Hello, salut!'
+    end
+    def alta_ruta
+        # render html: 'Ai intrat pe alta ruta!'
+    end
+end 
